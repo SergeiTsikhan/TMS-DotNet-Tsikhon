62deeef9cd6ab2f677119055480047141ec225c0
Gefgs <62507867+Gefgs@users.noreply.github.com>
2020-10-20 10:45:51 +0300

    :fix update homework 4-5

---
+++ Homework6/Imanager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework6
+{
+    interface Imanager
+    {
+        List<AnimalBase<string>> Animal { get; set; }
+        void Show();
+    }
+}
---
+++ scr/Homework4/Status.cs
@@ -0,0 +1,10 @@
+﻿namespace Vidos5_class_oop
+{
+    public enum Status
+    {
+        Unknown = 0,
+        Done = 1,
+        not_ready = 2,
+        inprocesed = 3
+    }
+}
